0d2fd8272c9d752c51aff17b70caf62343ffc157
Ron Apelbaum <ron.apelbaum@hpe.com>
2016-08-25 13:44:00 +0300

    tour of heroes p3

--- src/app/heroes/heroes.components.ts
+++ src/app/heroes/heroes.components.ts
@@ -1,9 +1,7 @@
 import {Component} from "@angular/core";
+import {Hero} from "./hero";
+import {HeroDetailComponent} from "./hero-detail.component";
 
-export class Hero {
-    id:number;
-    name:string;
-}
 
 const HEROES: Hero[] = [
     { id: 11, name: 'Mr. Nice' },
@@ -28,16 +26,12 @@ const HEROES: Hero[] = [
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
               </li>
             </ul>
-            <div *ngIf="selectedHero">
-                <h2>{{selectedHero.name}} details!</h2>
-                <div><label>id: </label>{{selectedHero.id}}</div>
-                <div>
-                    <label>name: </label>
-                    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-                </div>
-            </div>
+            <my-hero-detail [hero]="selectedHero"></my-hero-detail>
             `,
     styleUrls: [ './heroes.css' ],
+    entryComponents: [
+        HeroDetailComponent
+    ],
 })
 export class HeroesComponent {
     heroes = HEROES;
